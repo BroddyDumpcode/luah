92db81db42e3566fb92076b244a7b358e891af33
broddy <palpalehot69@gmail.com>
2026-03-09 16:53:12 +0700

    Refactor module loading syntax in main.lua

--- main.lua
+++ main.lua
@@ -2,13 +2,14 @@ local repo = "https://raw.githubusercontent.com/BroddyDumpcode/"
 local gui = loadstring(game:HttpGet(repo.."luah/refs/heads/main/utils/GUI.lua"))()
 
 local modules = {
-    ngapung = loadstring(game:HttpGet(repo.."luah/refs/heads/main/modules/fly.lua"))()
-    ngabret = loadstring(game:HttpGet(repo.."luah/refs/heads/main/modules/ngabret.lua"))()
-    esp = loadstring(game:HttpGet(repo.."luah/refs/heads/main/modules/esp.lua"))()
-    pepet = loadstring(game:HttpGet(repo.."luah/refs/heads/main/utils/tpgui.lua"))()
-    nclip = loadstring(game:HttpGet(repo.."luah/refs/heads/main/modules/nclip.lua"))()
+    ngapung = loadstring(game:HttpGet(repo.."luah/refs/heads/main/modules/fly.lua"))(),
+    ngabret = loadstring(game:HttpGet(repo.."luah/refs/heads/main/modules/ngabret.lua"))(),
+    esp = loadstring(game:HttpGet(repo.."luah/refs/heads/main/modules/esp.lua"))(),
+    pepet = loadstring(game:HttpGet(repo.."luah/refs/heads/main/utils/tpgui.lua"))(),
+    nclip = loadstring(game:HttpGet(repo.."luah/refs/heads/main/modules/nclip.lua"))(),
     infjmp = loadstring(game:HttpGet(repo.."luah/refs/heads/main/modules/infjmp.lua"))()
 }
 
 
 gui:Init(modules)
+

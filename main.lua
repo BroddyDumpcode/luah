c00ac8e2b2413fa4458051f8f5935dafceb65aad
broddy <palpalehot69@gmail.com>
2026-03-11 03:07:23 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,26 +1,26 @@
-
-
 repeat task.wait() until game:IsLoaded()
 local repo = "https://raw.githubusercontent.com/BroddyDumpcode/luah/main/"
-setProgress(0.2)
+local loader = loadstring(game:HttpGet(repo.."utils/loading.lua"))()
+loader:Init()
+loader:SetProgress(0.2)
 local GUI = loadstring(game:HttpGet(repo.."utils/GUI.lua"))()
-setProgress(0.4)
+loader:SetProgress(0.4)
 local modules = {
     ngapung = loadstring(game:HttpGet(repo.."modules/fly.lua"))(),
     ngabret = loadstring(game:HttpGet(repo.."modules/ngabret.lua"))(),
-    esp = loadstring(game:HttpGet(repo.."modules/esp.lua"))(),
-    nclip = loadstring(game:HttpGet(repo.."modules/nclip.lua"))(),
-    infjmp = loadstring(game:HttpGet(repo.."modules/infjmp.lua"))(),
-    pepet = loadstring(game:HttpGet(repo.."utils/tpgui.lua"))()
+    esp = loadstring(game:HttpGet(repo.."modules/esp.lua"))()
 }
-setProgress(0.7)
-
+loader:SetProgress(0.7)
+modules.nclip = loadstring(game:HttpGet(repo.."modules/nclip.lua"))()
+modules.infjmp = loadstring(game:HttpGet(repo.."modules/infjmp.lua"))()
+modules.pepet = loadstring(game:HttpGet(repo.."utils/tpgui.lua"))()
+loader:SetProgress(1)
 print("feature has been loaded...")
 print("ngabret:", modules.ngabret)
 print("setSpeed:", modules.ngabret and modules.ngabret.setSpeed)
-setProgress(1)
 task.wait(0.5)
 loadingGui:Destroy()
 GUI:Init(modules)
+
 
 
